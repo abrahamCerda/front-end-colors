b24db70123e7ba432e42f90cefcdf60cfb8daa36
Abraham Cerda <abraham.cerda@usach.cl>
2020-07-30 07:16:07 -0400

    Login implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +12,23 @@ export class LoginComponent implements OnInit {
   password: string;
   rememberMe: boolean;
 
-  constructor() { }
+  constructor(private readonly loginService: LoginService) {}
 
   ngOnInit(): void {
   }
 
   onLogin(): void{
-
+    if (!this.email || this.email === '' || !this.password || this.password === ''){
+      return;
+    }
+    this.loginService.login(this.email, this.password, this.rememberMe)
+      .subscribe(loginResponse => {
+        /* Navegar a la siguiente pantalla*/
+        console.log(loginResponse);
+      }, error => {
+        /* Que hacer en caso de error*/
+        console.error(error);
+      });
   }
 
 }
