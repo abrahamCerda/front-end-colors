b2740b8f08d825f73bd471079cd424ef03c8e4cf
Abraham Cerda <abraham.cerda@usach.cl>
2020-07-30 03:11:43 -0400

    First implementation of login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+  password: string;
+  rememberMe: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(): void{
+
   }
 
 }
